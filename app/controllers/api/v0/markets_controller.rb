dcd5647cefc836b081d20c9832f28cc736aa886c
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-04 20:42:31 -0700

    changed message

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -9,7 +9,7 @@ class Api::V0::MarketsController < ApplicationController
     if market
       render json: market
     else 
-      render json: {error: "detail: Market #{params[:id]} not found"}
+      render json: {error: "detail: Couldn't find Market with id:: #{params[:id]}"}
     end
   end
 end
