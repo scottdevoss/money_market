2c1630f0a12fbadf09176cdfda9cf8df7c897a6e
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-05 15:47:25 -0700

    added and then removed seed data

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,6 +1,6 @@
 class Api::V0::MarketsController < ApplicationController
   def index
-    render json: Market.all
+    render json: MarketSerializer.new(Market.all)
   end
 
   def show
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -9,36 +9,37 @@ describe "Markets API" do
     expect(response).to be_successful
 
     markets = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(markets[:data].count).to eq(10)
 
-    expect(markets.count).to eq(10)
+    markets[:data].each do |market|
+      # require 'pry'; binding.pry
+      expect(market[:attributes]).to have_key(:name)
+      expect(market[:attributes][:name]).to be_a(String)
 
-    markets.each do |market|
-      expect(market).to have_key(:name)
-      expect(market[:name]).to be_a(String)
+      expect(market[:attributes]).to have_key(:street)
+      expect(market[:attributes][:street]).to be_a(String)
 
-      expect(market).to have_key(:street)
-      expect(market[:street]).to be_a(String)
+      expect(market[:attributes]).to have_key(:city)
+      expect(market[:attributes][:city]).to be_a(String)
 
-      expect(market).to have_key(:city)
-      expect(market[:city]).to be_a(String)
+      expect(market[:attributes]).to have_key(:county)
+      expect(market[:attributes][:county]).to be_a(String)
 
-      expect(market).to have_key(:county)
-      expect(market[:county]).to be_a(String)
+      expect(market[:attributes]).to have_key(:state)
+      expect(market[:attributes][:state]).to be_a(String)
 
-      expect(market).to have_key(:state)
-      expect(market[:state]).to be_a(String)
+      expect(market[:attributes]).to have_key(:zip)
+      expect(market[:attributes][:zip]).to be_a(String)
 
-      expect(market).to have_key(:zip)
-      expect(market[:zip]).to be_a(String)
+      expect(market[:attributes]).to have_key(:lat)
+      expect(market[:attributes][:lat]).to be_a(String)
 
-      expect(market).to have_key(:lat)
-      expect(market[:lat]).to be_a(String)
+      expect(market[:attributes]).to have_key(:lon)
+      expect(market[:attributes][:lon]).to be_a(String)
 
-      expect(market).to have_key(:lon)
-      expect(market[:lon]).to be_a(String)
-
-      expect(market).to have_key(:vendor_count)
-      expect(market[:vendor_count]).to be(nil)
+      expect(market[:attributes]).to have_key(:vendor_count)
+      expect(market[:attributes][:vendor_count]).to be(nil)
     end
   end
 
