b6eaf884a0bf96ceb68685c71965c3d8f2a9a444
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-04 20:28:26 -0700

    added show test

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -41,4 +41,44 @@ describe "Markets API" do
       expect(market[:vendor_count]).to be(nil)
     end
   end
+
+  it "can get one market by its id" do
+    id = create(:market).id
+  
+    get "/api/v1/markets/#{id}"
+  
+    market = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(response).to be_successful
+  
+    expect(market).to have_key(:id)
+    expect(market[:id]).to eq(id)
+  
+    expect(market).to have_key(:name)
+    expect(market[:name]).to be_a(String)
+  
+    expect(market).to have_key(:street)
+    expect(market[:street]).to be_a(String)
+
+    expect(market).to have_key(:city)
+    expect(market[:city]).to be_a(String)
+
+    expect(market).to have_key(:county)
+    expect(market[:county]).to be_a(String)
+
+    expect(market).to have_key(:state)
+    expect(market[:state]).to be_a(String)
+
+    expect(market).to have_key(:zip)
+    expect(market[:zip]).to be_a(String)
+
+    expect(market).to have_key(:lat)
+    expect(market[:lat]).to be_a(String)
+
+    expect(market).to have_key(:lon)
+    expect(market[:lon]).to be_a(String)
+
+    expect(market).to have_key(:vendor_count)
+    expect(market[:vendor_count]).to be_an(Integer)
+  end
 end
