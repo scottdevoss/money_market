bbeadad6ddef9063b69cd15606f175f902ad0945
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-06 11:39:31 -0700

    completed user story 5

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::VendorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   def index
     market = Market.find(params[:market_id])
@@ -20,6 +21,11 @@ class Api::V0::VendorsController < ApplicationController
   def not_found_response(exception)
     render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
       .serialize_json, status: :not_found
+  end
+
+  def record_invalid(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400))
+      .serialize_json, status: :bad_request
   end
 
   def vendor_params
