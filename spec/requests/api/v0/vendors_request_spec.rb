6f5853360bb180f05353903b26301eca917e7d19
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-05 20:21:43 -0700

    wrote first test

--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -60,7 +60,25 @@ describe "Vendors API" do
 
     expect(response).to be_successful
 
+    vendor = vendor[:data]
+
+    expect(vendor).to have_key(:id)
+    expect(vendor[:id]).to be_a(String)
+
+    expect(vendor[:attributes]).to have_key(:name)
+    expect(vendor[:attributes][:name]).to be_a(String)
+
+    expect(vendor[:attributes]).to have_key(:description)
+    expect(vendor[:attributes][:description]).to be_a(String)
+
+    expect(vendor[:attributes]).to have_key(:contact_name)
+    expect(vendor[:attributes][:contact_name]).to be_a(String)
+
+    expect(vendor[:attributes]).to have_key(:contact_phone)
+    expect(vendor[:attributes][:contact_phone]).to be_a(String)
+
+    expect(vendor[:attributes]).to have_key(:credit_accepted)
+    expect(vendor[:attributes][:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
     
-    require 'pry'; binding.pry
   end
 end
