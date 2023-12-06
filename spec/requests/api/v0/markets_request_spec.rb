d2ce2bfe418e4448c56db36ffa3ecd687f952087
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-05 17:52:30 -0700

    added first test for getting all vendors for a market

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -114,5 +114,27 @@ describe "Markets API" do
 
     vendors = vendors[:data]
 
+    expect(vendors.count).to eq(3)
+
+    vendors.each do |vendor|
+      expect(vendor).to have_key(:id)
+      expect(vendor[:id]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:name)
+      expect(vendor[:attributes][:name]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:description)
+      expect(vendor[:attributes][:description]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:contact_name)
+      expect(vendor[:attributes][:contact_name]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:contact_phone)
+      expect(vendor[:attributes][:contact_phone]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:credit_accepted)
+      expect(vendor[:attributes][:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
+      
+    end
   end
 end
