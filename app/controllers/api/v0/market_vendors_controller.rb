cc1d8ecb605d8031f6de13e93b020f9c186fdf02
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-07 13:09:51 -0700

    passed sad path delete vendor

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -4,10 +4,20 @@ class Api::V0::MarketVendorsController < ApplicationController
     render json: MarketVendorSerializer.new(market_vendor), status: 201
   end
 
+  # def destroy
+  #   # require 'pry'; binding.pry
+  #   market_vendor = MarketVendor.find(params[:id])
+  #   render json: MarketVendorSerializer.delete(market_vendor), status: 204
+  # end
+
   def destroy
-    # require 'pry'; binding.pry
-    market_vendor = MarketVendor.find(params[:id])
-    render json: MarketVendorSerializer.delete(market_vendor), status: 204
+    market_vendor = MarketVendor.find_by(market_vendor_params)
+    if market_vendor.nil?
+      render json: ErrorSerializer.new(ErrorMessage.new("No MarketVendor with market_id=#{market_vendor_params[:market_id]} AND vendor_id=#{market_vendor_params[:vendor_id]} exists", 404))
+        .serialize_json, status: :not_found
+    else
+      render json: MarketVendor.delete(market_vendor), status: 204
+    end
   end
 
   private
--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -24,7 +24,7 @@ describe "MarketVendors API" do
     market = create(:market)      
     vendor = create(:vendor)
     market_vendor = create(:market_vendor, market: market, vendor: vendor)
-    market_vendor_params = ({
+    market_vendor_info = ({
       market_id: market.id,
       vendor_id: vendor.id
     })
@@ -32,10 +32,32 @@ describe "MarketVendors API" do
 
     expect(MarketVendor.count).to eq(1)
 
-    delete '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+    delete '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: market_vendor_info)
 
     expect(response).to be_successful
+    expect(response.status).to eq(204)
     expect(MarketVendor.count).to eq(0)
     expect{MarketVendor.find(market_vendor.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "delete will gracefully handle if a market_vendor for given ids does not exist" do
+    market = create(:market)
+    vendor = create(:vendor)
+    market_vendor_info = ({
+                    market_id: market.id,
+                    vendor_id: vendor.id
+                  })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    delete '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: market_vendor_info)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq('404')
+    expect(data[:errors].first[:title]).to eq("No MarketVendor with market_id=#{market.id} AND vendor_id=#{vendor.id} exists")
   end
 end
